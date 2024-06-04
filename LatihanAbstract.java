700fb87a062b2f1b650f22ae0ede29928eb13790
waf4hmad <waf4hmad@gmail.com>
2024-06-04 09:37:24 +0700

    Membuat Class Abstract

---
+++ LatihanAbstract.java
@@ -0,0 +1,24 @@
+// Abstract class
+abstract class Animal {
+  // Abstract method (does not have a body)
+  public abstract void animalSound();
+  // Regular method
+  public void sleep() {
+    System.out.println("Zzz");
+  }
+}
+
+// Subclass (inherit from Animal)
+class Pig extends Animal {
+   public void animalSound(){
+   	System.out.println("aaa");
+   }
+}
+
+public class Main {
+  public static void main(String[] args) {
+    Pig myPig = new Pig(); // Create a Pig object
+    myPig.animalSound();
+    myPig.sleep();
+  }
+}
